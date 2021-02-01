7adc753309c84218634547133ebab7d9650f5214
Martin M <maroymart@gmail.com>
2021-02-01 16:22:04 -0500

    Added TableFlipFlop addon

---
+++ addons/tableflipflop.js
@@ -0,0 +1,30 @@
+const exports = module.exports;
+
+module.exports.message = (client, message) => {
+	// Anti-tableflip
+
+	if(message.endsWith('(╯°□°）╯︵ ┻━┻') && !message.author.bot) {
+		setTimeout(() => {
+			message.channel.send('┬─┬ ╯( ゜-゜╯)').then(m => {
+				setTimeout(() => {
+					m.edit('┬─┬ ノ( ゜-゜ノ)').then(l => {
+						setTimeout(() => {
+							l.edit('┬─┬ ノ( ^ ˽ ^ ノ)');
+						}, 500); // Stage 3
+					});
+				}, 700); // Stage 2
+			});
+		}, 200); // Stage 1
+	}
+
+
+	// Anti-unflip
+
+	if(message.endsWith('┬─┬ ノ( ゜-゜ノ)') && !message.author.bot) {
+		message.channel.send('(ட °˽°)ட  ︵ ┻━┻').then(m => {
+			setTimeout(() => {
+				m.edit('(╯°□°）╯︵ ┻━┻');
+			}, 400); // Stage 2
+		}); // Stage 1
+	}
+};
