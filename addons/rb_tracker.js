c69935f86838efe8ee1c05eb2353a8414e064732
Razboy20 <razboy20@gmail.com>
2021-02-02 10:04:46 -0600

    Update rb tracking to use Maps/collections

--- addons/rb_tracker.js
+++ addons/rb_tracker.js
@@ -2,16 +2,12 @@ var trackers = [];
 
 const schedule = require('node-schedule');
 const rule = new schedule.RecurrenceRule().hour = 0;
-const clearRollbacks = schedule.scheduleJob(rule, () => {
-    trackers = [];
-});
 
-function getTracker(userid) {
-    return trackers.filter(obj => {
-        return obj.id === userid;
-    });
-    return null;
-}
+const trackers = new Map();
+      
+const clearRollbacks = schedule.scheduleJob(rule, () => {
+    trackers.clear();
+});
 
 class Tracker {
     constructor(userid) {
@@ -19,20 +15,29 @@ class Tracker {
         this.rollbacks = 0;
     }
 }
+
 exports.message = (client, msg) => {
+    const user = msg.mentions.members.first();
+    
+    if (user) {
+        if (trackers.has(user.id)) {
+            tracker = trackers.get(user.id);
+        } else {
+            tracker = new Tracker(user.id);
+            trackers.set(user.id, tracker);
+        }
+    } else {
+        msg.channel.send("Invalid user.");
+        return;
+    }
+    
     var args = msg.content.toLowerCase().split(" ");
     if(args[0] == "!rb") {
         if(args[1] == "add") {
-            if(msg.member.roles.cache.find(r => r.id.toString() === '765034419509526549')) {
-                var user = msg.guild.member(msg.mentions.users.first());
+            if(msg.member.roles.cache.find(r => r.id == '765034419509526549')) {
+                var user = msg.mentions.members.first();
                 if(user) {
-                    var tracker = getTracker(user.id);
-                    if(tracker.length == 0) {
-                        tracker = new Tracker(user.id)
-                        trackers.push(tracker);
-                    } else {
-                        tracker = tracker[0]
-                    }
+                    const tracker = trackers.get(user.id);
                     tracker.rollbacks++;
                     msg.channel.send("Added a rollback to `" + user.user.tag + "`'s count.");
                 } else {
@@ -41,15 +46,14 @@ exports.message = (client, msg) => {
             } else {
                 msg.channel.send("You do not have permission to use this command.");
             }
-        } else if(args[1] == "remove") {
-            if(msg.member.roles.cache.find(r => r.id.toString() === '765034419509526549')) {
-                var user = msg.guild.member(msg.mentions.users.first());
+        } else if (args[1] == "remove") {
+            if(msg.member.roles.cache.find(r => r.id == '765034419509526549')) {
+                var user = msg.mentions.members.first();
                 if(user) {
-                    var tracker = getTracker(user.id);
-                    if(tracker.length == 0) {
+                    const tracker = trackers.get(user.id);
+                    if(!trackers.has(user.id)) {
                         msg.channel.send("`" + user.user.tag + "` has had no rollbacks in the past 24 hours.");
                     } else {
-                        tracker = tracker[0];
                         if(tracker.rollbacks > 0) {
                             tracker.rollbacks--;
                             msg.channel.send("Removed a rollback to `" + user.user.tag + "`'s count.");
@@ -65,11 +69,11 @@ exports.message = (client, msg) => {
                 msg.channel.send("You do not have permission to use this command.");
             }
         } else {
-            var user = msg.guild.member(msg.mentions.users.first()) || msg.member;
+            var user = msg.mentions.members.first() || msg.member;
             if(user) {
-                var tracker = getTracker(user.id);
-                if(tracker.length != 0) {
-                    msg.channel.send("`" + user.user.tag + "` has had " + tracker[0].rollbacks + " rollback(s) in the last 24 hours.");
+                const tracker = trackers.get(user.id);
+                if(trackers.has(user.id)) {
+                    msg.channel.send("`" + user.user.tag + "` has had " + tracker.rollbacks + " rollback(s) in the last 24 hours.");
                 } else {
                     msg.channel.send("`" + user.user.tag + "` has had no rollbacks in the past 24 hours.");
                 }
