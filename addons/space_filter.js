52ac8ea9df1f0190732f5fb60a505ae8313b782e
Martin M <maroymart@gmail.com>
2021-04-21 17:46:17 -0400

    Added space filter

---
+++ addons/space_filter.js
@@ -0,0 +1,13 @@
+module.exports.message = function(client, msg) {
+	const space_channel = "603336862878138408";
+	if(msg.channel.id != space_channel) return;
+	const split_msg = msg.content.split(" ");
+
+	split_msg.forEach(element => {
+		const filtered = element.replace(" ", "");
+
+		if(filtered.length > 1) {
+			return msg.delete();
+		}
+	});
+};
