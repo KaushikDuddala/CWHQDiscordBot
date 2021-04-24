28bd007bd65518b6e955bf718c226d67b2fc8dd0
Razboy20 <razboy20@gmail.com>
2021-04-23 21:27:09 -0500

    Update space_filter.js

--- addons/space_filter.js
+++ addons/space_filter.js
@@ -3,7 +3,7 @@ module.exports.message = function (client, msg) {
     if (msg.channel.id != space_channel) return;
     const split_msg = msg.content.split(" ");
 
-    split_msg.forEach(async (filtered) => {
+    split_msg.forEach(async filtered => {
         if (filtered.length > 1) {
             if (
                 !filtered.toLowerCase().startsWith("https://") &&
@@ -13,7 +13,7 @@ module.exports.message = function (client, msg) {
                 const _msg = await msg.reply("P l e a s e  s p a c e  y o u r  m e s s a g e s  h e r e .  T h a n k  Y o u !");
                 await msg.delete();
                 setTimeout(() => {
-                    await _msg.delete();
+                    _msg.delete();
                 }, 3000);
 
                 return;
